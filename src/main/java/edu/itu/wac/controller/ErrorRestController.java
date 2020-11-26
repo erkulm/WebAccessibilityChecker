af8d5d5f7e5734d6db3b9058a85e0b0b3210b26c
mahmut <erkul@itu.edu.tr>
2020-11-27 02:19:44 +0300

    error rest service

--- src/main/java/edu/itu/wac/controller/ErrorRestController.java
+++ src/main/java/edu/itu/wac/controller/ErrorRestController.java
@@ -1,10 +1,18 @@
 package edu.itu.wac.controller;
 
+import edu.itu.wac.entity.Error;
+import edu.itu.wac.entity.Website;
+import edu.itu.wac.entity.WebsiteCategory;
 import edu.itu.wac.service.ErrorService;
 import edu.itu.wac.service.WebsiteCategoryService;
 import edu.itu.wac.service.WebsiteService;
+import edu.itu.wac.service.request.ErrorRequest;
+import edu.itu.wac.service.request.WebsiteCategoryRequest;
+import edu.itu.wac.service.request.WebsiteRequest;
 import edu.itu.wac.service.response.ErrorResponse;
+import edu.itu.wac.service.response.WebsiteCategoryResponse;
 import edu.itu.wac.service.response.WebsiteResponse;
+import edu.itu.wac.util.Pa11yUtil;
 import ma.glasnost.orika.MapperFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -36,7 +44,28 @@ public class ErrorRestController {
 
     @GetMapping("/error-report")
     @ResponseBody
-    List<ErrorResponse> getWebsiteByAddress(@RequestParam @NotNull String address){
+    List<ErrorResponse> getWebsiteByAddress(@RequestParam @NotNull String address) {
+        return errorService.findByWebsiteAddress(address);
+    }
+
+    @GetMapping("/generate-report")
+    @ResponseBody
+    List<ErrorResponse> generateNewReport(@RequestParam @NotNull String address) {
+        WebsiteResponse websiteResponse = websiteService.findByAddress(address);
+        if (websiteResponse == null){
+            WebsiteRequest websiteRequest = new WebsiteRequest();
+            websiteRequest.setAddress(address);
+            WebsiteCategoryRequest category = new WebsiteCategoryRequest();
+            category.setName("general");
+            WebsiteCategoryResponse savedCategory = websiteCategoryService.save(category);
+            websiteRequest.setCategory(mapperFacade.map(savedCategory, WebsiteCategory.class));
+            websiteResponse = websiteService.save(websiteRequest);
+
+        }
+        List<Error> errors = Pa11yUtil.runPa11y(
+                               mapperFacade.map(websiteResponse, Website.class),
+                          "", websiteResponse.getCategory());
+        errors.forEach(error -> errorService.save(mapperFacade.map(error, ErrorRequest.class)));
         return errorService.findByWebsiteAddress(address);
     }
 }
